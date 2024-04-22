6fe7ed4e9d886e2afb000bc6d704bc4ec839871b
Takayama Fumihiko <tekezo@pqrs.org>
2024-04-23 07:37:51 +0900

    Version 5.0.0

--- include/pqrs/osx/iokit_hid_device.hpp
+++ include/pqrs/osx/iokit_hid_device.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_hid_device v4.2
+// pqrs::osx::iokit_hid_device v5.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
